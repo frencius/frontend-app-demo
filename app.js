5d53d8058787520d82cddbfb8a8d157332cb2c56
frencius <frenciusleonardu@yahoo.com>
2017-09-12 21:04:59 +0700

    [ADD-FEATURE] simple crud view

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-test
+var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var path = require('path');
+
+var app = express();
+
+var port = 8080;
+var title = "Example Demo";
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/resources/views'));
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/resources/public'));
+
+app.listen(port, function() {
+  console.log('app started in port ' + port);
+});
+
+app.get('/', function(req, res) {
+  res.render('pages/index',     
+  		{
+  			layout:'layouts/master',
+  			title : title,
+  		}
+  	);
+});
+
+app.get('/simple-crud', function(req, res) {
+  res.render('pages/simple-crud/index',     
+  		{
+  			layout:'layouts/master',
+  			title : title,
+  		}
+  	);
+});
+
+app.get('/simple-crud/create', function(req, res) {
+  res.render('pages/simple-crud/create',     
+  		{
+  			layout:'layouts/master',
+  			title : title,
+  		}
+  	);
+});
